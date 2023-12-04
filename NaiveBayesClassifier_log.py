b1ef45b616cfb3da8f18d0908181697dc5c539a5
vaticanguy <krystian.szabat@outlook.com>
2023-12-04 19:15:28 +0100

    coś działa, trzeba zrobić refactoring do log zeby nie świeciło na czerwono u Dżaka

--- NaiveBayesClassifier_log.py
+++ NaiveBayesClassifier_log.py
@@ -23,78 +23,76 @@ def discretize_categorical_data(data):
 
     label_encoder = LabelEncoder()
 
+    # Pętla po kolumnach
     for col_index in range(encoded_data.shape[1]):
         encoded_data[:, col_index] = label_encoder.fit_transform(encoded_data[:, col_index])
 
     return encoded_data
 
-
-
-class NaiveBayesClassifier(BaseEstimator, ClassifierMixin):
+class NaiveBayesClassifier_log(BaseEstimator, ClassifierMixin):
     def __init__(self, laplace_smoothing=False):
         self.laplace_smoothing = laplace_smoothing
         self.classes = None
-        self.feature_probs = dict()
+        self.feature_log_probs = dict()
 
     @staticmethod
-    def default_prob():
-        return 1e-5
+    def default_log_prob():
+        return np.log(1e-5)
 
     def fit(self, X, y):
         labels, counts = np.unique(y, return_counts=True)
         priors = counts / y.size
-        self.classes = dict(zip(labels, priors))
+        log_priors = np.log(priors)
+        self.classes = dict(zip(labels, log_priors))
         number_of_features = X.shape[1]
         feature_indices = np.arange(number_of_features)
         for label in labels:
             labeldata = X[y == label]
             number_of_labelsamples = labeldata.shape[0]
-            self.feature_probs[label] = dict()
+            self.feature_log_probs[label] = dict()
             for feature_index in feature_indices:
                 feature_values, feature_counts = np.unique(labeldata[:, feature_index], return_counts=True)
 
                 if self.laplace_smoothing:
-                    feature_probs = (feature_counts + 1) / (number_of_labelsamples + feature_values.size)
+                    feature_probs = np.log((feature_counts + 1) / (number_of_labelsamples + feature_values.size))
                 else:
-                    feature_probs = feature_counts / number_of_labelsamples
+                    feature_probs = np.log(feature_counts / number_of_labelsamples)
  
-                self.feature_probs[label][feature_index] = defaultdict(self.default_prob, zip(feature_values, feature_probs))
+                self.feature_log_probs[label][feature_index] = defaultdict(self.default_log_prob, zip(feature_values, feature_probs))
 
         return self
-    
 
-    
-    def predict_proba(self, X):
+    def predict_log_proba(self, X):
         number_of_samples = X.shape[0]
         number_of_features = X.shape[1]
         feature_indices = np.arange(number_of_features)
         number_of_classes = len(self.classes.keys())
 
-        predictions = np.empty((number_of_samples, number_of_classes))
+        log_predictions = np.empty((number_of_samples, number_of_classes))
 
         for sample_index, sample in enumerate(X):
             for label_index, label in enumerate(self.classes.keys()):
-                prior = self.classes[label]
-                feature_probs = self.feature_probs[label]
+                log_prior = self.classes[label]
+                log_likelihood = 0
+                feature_log_probs = self.feature_log_probs[label]
 
                 for feature_index in feature_indices:
                     feature_value = sample[feature_index]
 
-                    # już nie jest potrzebne bo jest defaultdict
-                    # if feature_value in feature_probs[feature_index]:
-                    #     feature_prob = feature_probs[feature_index][feature_value]
-                    # else:
-                    #     feature_prob = 1e-5
+                    log_feature_prob = feature_log_probs[feature_index][feature_value]
+                    log_likelihood += log_feature_prob
 
-                    feature_prob = feature_probs[feature_index][feature_value]
-                    prior *= feature_prob
+                log_predictions[sample_index, label_index] = log_prior + log_likelihood
 
-                predictions[sample_index, label_index] = prior
- 
+        return log_predictions
+
+    def predict_proba(self, X):
+        log_predictions = self.predict_log_proba(X)
+        predictions = np.exp(log_predictions - np.max(log_predictions, axis=1).reshape(-1, 1)) / np.sum(np.exp(log_predictions - np.max(log_predictions, axis=1).reshape(-1, 1)), axis=1).reshape(-1, 1)
 
         return predictions
 
     def predict(self, X):
-        predictions = self.predict_proba(X)
+        log_predictions = self.predict_log_proba(X)
         labels = np.array(list(self.classes.keys()))
-        return labels[np.argmax(predictions, axis=1)]
+        return labels[np.argmax(log_predictions, axis=1)]
--- main_mushroom.py
+++ main_mushroom.py
@@ -1,5 +1,6 @@
 import numpy as np
 from NaiveBayesClassifier import NaiveBayesClassifier, score, discretize_categorical_data
+from NaiveBayesClassifier_log import NaiveBayesClassifier_log
 from sklearn.model_selection import train_test_split
 
 data = np.genfromtxt('Datasets/Mushroom/agaricus-lepiota.data', delimiter=',', dtype='U1') # na koniec zmienić na S1 i spr czy działa
@@ -10,7 +11,7 @@ y = data[:, 0]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2105)
 
-nbc = NaiveBayesClassifier(laplace_smoothing=True)
+nbc = NaiveBayesClassifier_log(laplace_smoothing=True)
 nbc.fit(X_train, y_train)
 
 predictions = nbc.predict(X_test)
